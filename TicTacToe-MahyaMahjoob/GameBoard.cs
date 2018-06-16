eef268ca2e963cdf264091df068f8ba9d8fd8cc9
Smpl-Ubuntu <mahjoub.mahya>
2018-06-16 15:28:33 +0430

    1st Commit

---
+++ TicTacToe-MahyaMahjoob/GameBoard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_MahyaMahjoob
+{
+    public enum Player
+    {
+        X = 1,
+        O = -1,
+        Open = 0,
+    }
+    public abstract class GameBoard
+    {
+        public Player[,] squares;
+        public abstract Player this[int x, int y] { get; set; }
+        public abstract bool IsFull { get; }
+        public abstract int Size { get; }
+    }
+}
