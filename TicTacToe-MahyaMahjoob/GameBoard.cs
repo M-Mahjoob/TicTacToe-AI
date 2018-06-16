ce064002d813ea3ab57098906cd19545132d93ba
M-Mahjoob <mhymhj01@gmail.com>
2018-06-16 20:58:35 +0430

    1st

--- TicTacToe-MahyaMahjoob/GameBoard.cs
+++ TicTacToe-MahyaMahjoob/GameBoard.cs
@@ -19,6 +19,7 @@ namespace TicTacToe_MahyaMahjoob
         public abstract bool IsFull { get; }
         public abstract int Size { get; }
         public abstract List<Space> OpenSquares { get; }
+        public abstract Player Winner { get; }
         public abstract GameBoard Clone();
     }
     public struct Space
